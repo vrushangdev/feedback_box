a775148643cebf4b7259bd019b3405a379c7421e
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-01-12 12:59:42 +0100

    Implement CreateInboxCommand

---
+++ feedback_box/application/inbox/commands/create.py
@@ -0,0 +1,17 @@
+from feedback_box.domain.inbox import Inbox
+from feedback_box.application.interfaces.idatabase_service import DatabaseService
+
+
+class CreateInboxCommand:
+    def __init__(self, db: DatabaseService) -> None:
+        self.db = db
+
+    def execute(self, owner: str, password: str, question: str) -> Inbox:
+        new_inbox = self.db.create_inbox(
+            {
+                'owner': owner,
+                'password': password,
+                'question': question
+            }
+        )
+        return new_inbox
